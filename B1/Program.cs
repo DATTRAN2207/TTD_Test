5d738c6be78bbe4a902632c97d9284c173ab24fb
DATTRAN2207 <datvu2207@gmail.com>
2023-06-21 15:07:09 +0700

    Upload B1

---
+++ B1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] mangBanDau = {1,2,3,4,5};
+        int doiChieu = -3;
+        mangBanDau = DichChuyenMang(mangBanDau, doiChieu);
+        foreach (int i in mangBanDau)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    static int[] DichChuyenMang(int[] mang, int doiChieu)
+    {
+        int doDai = mang.Length;
+        int[] mangDichChuyen = new int[doDai];
+        for(int i = 0; i < doDai; i++)
+        {
+            int chiSoDichChuyen = (i + doiChieu) % doDai;
+            if(chiSoDichChuyen < 0)
+            {
+                chiSoDichChuyen += doDai;
+            }
+            mangDichChuyen[i] = mang[chiSoDichChuyen];
+        }
+        return mangDichChuyen;
+    }
+}
